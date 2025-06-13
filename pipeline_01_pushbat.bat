f1e861418ae231e250b0e98391d6ad5b4bc05535
disapear1997 <35909397+disapear1997@users.noreply.github.com>
2025-06-13 15:15:25 +0800

    second commit

---
+++ pipeline_01_pushbat.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Ask for commit message
+set /p commitMessage="Enter commit message: "
+
+git add .
+
+::Commit changes
+git commit -m "%commitMessage%"
+
+::Confirm remote repository
+git remote -v
+
+::Push changes to remote repository
+git push origin main
+
+
+pause
